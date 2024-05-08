9e79c747d1cdf9654fb643a42552bd707aff6cb0
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-08 17:11:52 +0545

    Remove: import traces

--- src/test_cases/entities/test_case.entity.ts
+++ src/test_cases/entities/test_case.entity.ts
@@ -1,6 +1,4 @@
 import { BaseEntity } from "src/entities/base.entity";
-import { LabReport } from "src/lab_reports/entities/lab_report.entity";
-import { TestCaseResult } from "src/types/global.types";
 import { Column, Entity, ManyToOne, OneToMany, OneToOne } from "typeorm";
 import { TestResult } from "./test_result.entity";
 
