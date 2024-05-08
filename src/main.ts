570147b8aac6779bf25d057140ed2a4118c8b0ea
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-08 23:20:59 +0545

    Add: security headers middlware

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,11 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './all-exceptions.filter';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { OrganizationsModule } from './organizations/organizations.module';
-import { DonationsModule } from './donations/donations.module';
-import { DonationEventsModule } from './donation_events/donation_events.module';
-import { CertificateModule } from './certificate/certificate.module';
-import { DonorsModule } from './donors/donors.module';
-import { AddressModule } from './address/address.module';
-import { DonorCardModule } from './donor_card/donor_card.module';
-import { LabReportsModule } from './lab_reports/lab_reports.module';
-import { TestCasesModule } from './test_cases/test_cases.module';
-import { VolunteersModule } from './volunteers/volunteers.module';
 import { setupSwagger } from './config/swagger.config';
 import helmet from 'helmet';
+import { SecurityHeadersMiddleware } from './middlewares/security_headers.middleware';
 const PORT = process.env.PORT || 3001;
 
 async function bootstrap() {
@@ -31,7 +19,11 @@ async function bootstrap() {
   });
 
   app.use(helmet()); // header security
-  
+
+  // security header middleware
+  const securityHeadersMiddleware = new SecurityHeadersMiddleware();
+  app.use(securityHeadersMiddleware.use.bind(securityHeadersMiddleware));
+
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.use(cookieParser());
