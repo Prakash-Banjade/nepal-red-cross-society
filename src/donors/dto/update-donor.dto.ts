fa8e8bc0017dc39c5fae6c69a766d7327ae5a19a
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-09 14:04:51 +0545

    Remove: remove omit type

--- src/donors/dto/update-donor.dto.ts
+++ src/donors/dto/update-donor.dto.ts
@@ -1,4 +1,5 @@
 import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateDonorDto } from './create-donor.dto';
 
-export class UpdateDonorDto extends PartialType(OmitType(CreateDonorDto, ['password'] as const)) { }
+// export class UpdateDonorDto extends PartialType(OmitType(CreateDonorDto, ['password'] as const)) { }
+export class UpdateDonorDto extends PartialType(CreateDonorDto) { }
