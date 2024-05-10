3f2a30277c3a0b8a89838bb7b2c2e272cc4d3375
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-10 13:16:30 +0545

    add image on update

--- src/donors/dto/update-donor.dto.ts
+++ src/donors/dto/update-donor.dto.ts
@@ -1,5 +1,10 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateDonorDto } from './create-donor.dto';
+import { FileSystemStoredFile } from 'nestjs-form-data';
+import { IsOptional } from 'class-validator';
 
-// export class UpdateDonorDto extends PartialType(OmitType(CreateDonorDto, ['password'] as const)) { }
-export class UpdateDonorDto extends PartialType(CreateDonorDto) { }
+export class UpdateDonorDto extends PartialType(CreateDonorDto) {
+    @ApiPropertyOptional({ type: 'string', format: 'binary', description: 'Donor image' })
+    @IsOptional()
+    image?: FileSystemStoredFile | string;
+}
