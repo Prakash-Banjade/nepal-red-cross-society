c1412c29002768c7ce16ff63573170696b1e95a1
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-10 13:55:59 +0545

    Fix: validate fields based on country

--- src/address/dto/create-address.dto.ts
+++ src/address/dto/create-address.dto.ts
@@ -12,24 +12,24 @@ export class CreateAddressDto {
 
     @ApiPropertyOptional({ type: 'enum', enum: Province, description: 'Province' })
     @IsEnum(Province, { message: 'Invalid province. Province must be either Koshi, Madhesh, Bagmati, Gandaki, Lumbini, Karnali, Sudurpashchim.' })
-    @ValidateIf(function () { return this.country === 'Nepal' })
+    @ValidateIf((object) => object.country === Country.NP) // field is required only if country is Nepal
     province?: Province;
 
     @ApiPropertyOptional({ type: 'enum', enum: District, description: 'District' })
     @IsEnum(District, { message: 'Invalid district.' })
-    @ValidateIf(function () { return this.country === 'Nepal' })
+    @ValidateIf((object) => object.country === Country.NP) // field is required only if country is Nepal
     district?: District;
 
     @ApiPropertyOptional({ type: 'enum', enum: Municipal, description: 'Municipality' })
     @IsEnum(Municipal, { message: 'Invalid municipality.' })
-    @ValidateIf(function () { return this.country === 'Nepal' })
+    @ValidateIf((object) => object.country === Country.NP) // field is required only if country is Nepal
     municipality?: Municipal;
 
     @ApiPropertyOptional({ description: 'Ward number', default: 1 })
     @IsInt()
     @Min(1)
-    @ValidateIf(function () { return this.country === 'Nepal' })
-    ward?: number = 1;
+    @ValidateIf((object) => object.country === Country.NP) // field is required only if country is Nepal
+    ward?: number;
 
     @ApiProperty({ description: 'Street name' })
     @IsString()
