31fc81936170a569139ff389a89e5268fe431bb0
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-14 17:20:12 +0545

    remove import trace

--- src/donors/entities/donor.entity.ts
+++ src/donors/entities/donor.entity.ts
@@ -3,7 +3,6 @@ import { Donation } from "src/donations/entities/donation.entity";
 import { DonorCard } from "src/donor_card/entities/donor_card.entity";
 import { BaseEntity } from "src/core/entities/base.entity";
 import { BloodType, Caste, Gender, Race, Religion, RhFactor } from "src/core/types/global.types";
-import * as bcrypt from 'bcrypt';
 import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, OneToOne } from "typeorm";
 
 @Entity()
