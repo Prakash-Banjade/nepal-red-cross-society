6e782b048b3ef75d74f3759fb6c75f81f550781a
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-15 17:28:25 +0545

    include full image url on filename

--- src/core/utils/getImageUrl.ts
+++ src/core/utils/getImageUrl.ts
@@ -1,9 +1,14 @@
 import { FileSystemStoredFile } from "nestjs-form-data";
+require('dotenv').config();
 
-export default function getFileName(file: FileSystemStoredFile | string) {
+export default function getFileName(file: FileSystemStoredFile | string | undefined) {
+    if (!file) return null;
+
     if (file instanceof FileSystemStoredFile) {
         const pathSegments = file?.path.split('\\');
         const fileName = pathSegments[pathSegments.length - 1];
-        return fileName;
+        const imageName = fileName?.split('/').at(-1);
+
+        return process.env.BACKEND_URL + '/public/' + imageName;
     } else return file;
 }
