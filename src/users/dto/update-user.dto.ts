53921ee28d08e104f518a07c4520cae5bbab3967
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-16 14:47:26 +0545

    keys made optional

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -6,19 +6,19 @@ export class UpdateUserDto {
     @ApiPropertyOptional({ type: 'string', description: 'firstName' })
     @IsString()
     @IsOptional()
-    firstName: string;
+    firstName?: string;
 
     @ApiPropertyOptional({ type: 'string', description: 'lastName' })
     @IsString()
     @IsOptional()
-    lastName: string;
+    lastName?: string;
 
     @ApiPropertyOptional({ type: 'string', description: 'Email' })
     @IsEmail()
     @IsOptional()
-    email: string;
+    email?: string;
 
     @ApiPropertyOptional({ type: 'string', format: 'binary', description: 'Profile Image' })
     @IsOptional()
-    image: string | FileSystemStoredFile
+    image?: string | FileSystemStoredFile
 }
