324cbb128121381aa766a56e67c171ffa18dad6a
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-16 15:37:46 +0545

    add: logo update

--- src/organizations/dto/update-organization.dto.ts
+++ src/organizations/dto/update-organization.dto.ts
@@ -1,9 +1,14 @@
-import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional, OmitType, PartialType } from '@nestjs/swagger';
 import { CreateOrganizationDto } from './create-organization.dto';
-import { IsUUID } from 'class-validator';
+import { IsOptional, IsUUID } from 'class-validator';
+import { FileSystemStoredFile } from 'nestjs-form-data';
 
-export class UpdateOrganizationDto extends PartialType(CreateOrganizationDto) {
+export class UpdateOrganizationDto extends PartialType(OmitType(CreateOrganizationDto, ['logo'])) {
     @ApiPropertyOptional({ type: String, isArray: true, format: 'uuid' })
     @IsUUID('4', { each: true })
+    @IsOptional()
     donations?: string[]
+
+    @ApiProperty({ type: 'file', format: 'binary', description: 'Donor image' })
+    logo: FileSystemStoredFile | string;
 }
