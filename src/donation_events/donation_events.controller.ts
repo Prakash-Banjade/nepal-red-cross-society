5d1316a476238b48054ff94f5a7629e6f3494197
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-19 16:30:45 +0545

    finalize donation_event

--- src/donation_events/donation_events.controller.ts
+++ src/donation_events/donation_events.controller.ts
@@ -5,14 +5,21 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
   ParseUUIDPipe,
+  Query,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { DonationEventsService } from './donation_events.service';
 import { CreateDonationEventDto } from './dto/create-donation_event.dto';
 import { UpdateDonationEventDto } from './dto/update-donation_event.dto';
 import { ApiBearerAuth, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { FileSystemStoredFile, FormDataRequest } from 'nestjs-form-data';
+import { ChekcAbilities } from 'src/core/decorators/abilities.decorator';
+import { Action } from 'src/core/types/global.types';
+import { ApiPaginatedResponse } from 'src/core/decorators/apiPaginatedResponse.decorator';
+import { QueryDto } from 'src/core/dto/queryDto';
+import { DonationEvent } from './entities/donation_event.entity';
 
 @ApiTags('Donation Event')
 @ApiBearerAuth()
@@ -24,18 +31,22 @@ export class DonationEventsController {
   @ApiConsumes('multipart/form-data')
   @FormDataRequest({ storage: FileSystemStoredFile })
   @ApiOperation({ description: "Add a new donation event", summary: "Create new donation event" })
+  @ChekcAbilities({ action: Action.CREATE, subject: 'all' })
   create(@Body() createDonationEventDto: CreateDonationEventDto) {
     return this.donationEventsService.create(createDonationEventDto);
   }
 
   @Get()
   @ApiOperation({ description: "Get all donation events", summary: "View donation events" })
-  findAll() {
-    return this.donationEventsService.findAll();
+  @ApiPaginatedResponse(CreateDonationEventDto)
+  @ChekcAbilities({ action: Action.READ, subject: DonationEvent })
+  findAll(@Query() queryDto: QueryDto) {
+    return this.donationEventsService.findAll(queryDto);
   }
 
   @Get(':id')
   @ApiOperation({ description: "Get a single donation event", summary: "View a single donation event" })
+  @ChekcAbilities({ action: Action.READ, subject: DonationEvent })
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.donationEventsService.findOne(id);
   }
@@ -44,6 +55,7 @@ export class DonationEventsController {
   @FormDataRequest({ storage: FileSystemStoredFile })
   @ApiOperation({ description: "Update a donation event", summary: "Edit existing donation event" })
   @ApiConsumes('multipart/form-data')
+  @ChekcAbilities({ action: Action.UPDATE, subject: 'all' })
   update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateDonationEventDto: UpdateDonationEventDto,
@@ -52,8 +64,23 @@ export class DonationEventsController {
   }
 
   @Post('deleteMany')
-  @ApiOperation({ description: "Delete a donation event", summary: "Delete a donation event" })
-  remove(@Body('ids') ids: string[]) {
-    return this.donationEventsService.remove(ids);
+  @HttpCode(HttpStatus.OK)
+  @ChekcAbilities({ action: Action.DELETE, subject: 'all' })
+  remove(@Body('ids') ids: string) {
+    return this.donationEventsService.remove(JSON.parse(ids));
+  }
+
+  @Post('restoreMany')
+  @ChekcAbilities({ action: Action.RESTORE, subject: 'all' })
+  @HttpCode(HttpStatus.OK)
+  restore(@Body('ids') ids: string) {
+    return this.donationEventsService.restore(JSON.parse(ids));
+  }
+
+  @Post('emptyTrash')
+  @HttpCode(HttpStatus.OK)
+  @ChekcAbilities({ action: Action.DELETE, subject: 'all' })
+  emptyTrash() {
+    return this.donationEventsService.clearTrash();
   }
 }
--- src/donation_events/donation_events.service.ts
+++ src/donation_events/donation_events.service.ts
@@ -1,12 +1,17 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDonationEventDto } from './dto/create-donation_event.dto';
 import { UpdateDonationEventDto } from './dto/update-donation_event.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DonationEvent } from './entities/donation_event.entity';
-import { In, Repository } from 'typeorm';
+import { In, IsNull, Not, Or, Repository } from 'typeorm';
 import { Volunteer } from 'src/volunteers/entities/volunteer.entity';
 import { OrganizationsService } from 'src/organizations/organizations.service';
 import { AddressService } from 'src/address/address.service';
+import { extractAddress } from 'src/core/utils/extractAddress';
+import { Deleted, QueryDto } from 'src/core/dto/queryDto';
+import paginatedData from 'src/core/utils/paginatedData';
+import getFileName from 'src/core/utils/getImageUrl';
+import { FileSystemStoredFile } from 'nestjs-form-data';
 
 @Injectable()
 export class DonationEventsService {
@@ -20,38 +25,58 @@ export class DonationEventsService {
 
   async create(createDonationEventDto: CreateDonationEventDto) {
     // retrieving volunteers
-    const volunteers = await this.volunteersRepo.find({
+    const volunteers = createDonationEventDto.volunteers ? await this.volunteersRepo.find({
       where: {
         id: In(createDonationEventDto.volunteers),
       },
-    });
+    }) : null;
 
     // retrieving organization
     const organization = await this.organizationsService.findOne(createDonationEventDto.organization)
 
     // retrieving address
-    const address = await this.addressService.create(this.extractAddress(createDonationEventDto));
+    const address = await this.addressService.create(extractAddress(createDonationEventDto));
+
+    // retrieving cover image
+    const coverImage = createDonationEventDto.coverImage ? getFileName(createDonationEventDto.coverImage) : null;
 
     const donationEvent = this.donationEventsRepo.create({
       ...createDonationEventDto,
-      address: address,
-      organization: organization,
-      volunteers: volunteers,
-      gallery: [], // TODO: implement gallery
-      coverImage: '', // TODO: implement cover image
+      address,
+      organization,
+      volunteers,
+      coverImage,
     })
 
     return await this.donationEventsRepo.save(donationEvent);
   }
 
-  async findAll() {
-    return await this.donationEventsRepo.find();
+  async findAll(queryDto: QueryDto) {
+    const queryBuilder = this.donationEventsRepo.createQueryBuilder('donationEvent');
+    const deletedAt = queryDto.deleted === Deleted.ONLY ? Not(IsNull()) : queryDto.deleted === Deleted.NONE ? IsNull() : Or(IsNull(), Not(IsNull()));
+
+    queryBuilder
+      .orderBy("donationEvent.createdAt", queryDto.order)
+      .skip(queryDto.skip)
+      .take(queryDto.take)
+      .withDeleted()
+      .where({ deletedAt })
+      .leftJoinAndSelect('donationEvent.address', 'address')
+      .leftJoinAndSelect('donationEvent.organization', 'organization')
+      .getMany()
+
+    return paginatedData(queryDto, queryBuilder);
   }
 
   async findOne(id: string) {
     const existingEvent = await this.donationEventsRepo.findOne({
       where: { id },
-      relations: { address: true },
+      relations: {
+        address: true,
+        volunteers: true,
+        donations: true,
+        organization: true
+      },
     });
     if (!existingEvent) throw new NotFoundException('Event not found');
 
@@ -62,46 +87,67 @@ export class DonationEventsService {
     const existingEvent = await this.findOne(id);
 
     // setting if address is updated
-    await this.addressService.update(existingEvent.address.id, this.extractAddress(updateDonationEventDto))
+    await this.addressService.update(existingEvent.address.id, extractAddress(updateDonationEventDto))
 
     // retrieving volunteers
-    const volunteers = await this.volunteersRepo.find({
+    const volunteers = updateDonationEventDto.volunteers ? await this.volunteersRepo.find({
       where: {
         id: In(updateDonationEventDto.volunteers),
       },
-    });
+    }) : existingEvent.volunteers
 
     // retrieving organization
     const organization = updateDonationEventDto.organization ? await this.organizationsService.findOne(updateDonationEventDto.organization) : existingEvent.organization
+
+    // retrieving cover image
+    const coverImage = updateDonationEventDto.coverImage ? getFileName(updateDonationEventDto.coverImage) : existingEvent.coverImage
+
+    // retrieving gallery
+    const gallery = updateDonationEventDto?.gallery ? this.getGalleryUrls(updateDonationEventDto.gallery) : existingEvent.gallery
 
     Object.assign(existingEvent, {
       ...updateDonationEventDto,
       volunteers: volunteers,
       organization,
+      coverImage,
+      gallery,
     });
 
-    // TODO: implement gallery, cover image
-
-    return await this.donationEventsRepo.save(existingEvent);    
+    return await this.donationEventsRepo.save(existingEvent);
   }
 
   async remove(ids: string[]) {
-    const existingEvents = await this.donationEventsRepo.find({
+    const foundDonationEvents = await this.donationEventsRepo.find({
       where: {
-        id: In(ids),
-      },
-    });
-    await this.donationEventsRepo.softRemove(existingEvents);
+        id: In(ids)
+      }
+    })
+    await this.donationEventsRepo.softRemove(foundDonationEvents);
 
     return {
       success: true,
-      message: 'Events deleted successfully',
+      message: 'DonationEvents deleted successfully',
     }
   }
 
-  public extractAddress(dto: CreateDonationEventDto | UpdateDonationEventDto) {
-    const { country, province, district, municipality, ward, street } = dto;
-    return { country, province, district, municipality, ward, street };
+  async restore(ids: string[]) {
+    const existingDonationEvents = await this.donationEventsRepo.find({
+      where: { id: In(ids) },
+      withDeleted: true,
+    })
+    if (!existingDonationEvents) throw new BadRequestException('DonationEvent not found');
+
+    return await this.donationEventsRepo.restore(ids);
+  }
+
+  async clearTrash() {
+    return await this.donationEventsRepo.delete({
+      deletedAt: Not(IsNull())
+    })
+  }
+
+  private getGalleryUrls(gallery: FileSystemStoredFile[]) {
+    return gallery.map(image => getFileName(image))
   }
 
 }
