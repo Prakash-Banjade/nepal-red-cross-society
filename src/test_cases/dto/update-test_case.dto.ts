4ae1cfb8980d55e4becfb08834914fc8e3258980
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-21 16:09:19 +0545

    remove lab report on test case update

--- src/test_cases/dto/update-test_case.dto.ts
+++ src/test_cases/dto/update-test_case.dto.ts
@@ -2,9 +2,4 @@ import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateTestCaseDto } from './create-test_case.dto';
 import { IsNotEmpty, IsUUID } from 'class-validator';
 
-export class UpdateTestCaseDto extends PartialType(CreateTestCaseDto) {
-    @ApiPropertyOptional({ format: 'uuidv4', type: 'string' })
-    @IsUUID()
-    @IsNotEmpty()
-    labReport: string;
-}
+export class UpdateTestCaseDto extends PartialType(CreateTestCaseDto) {}
