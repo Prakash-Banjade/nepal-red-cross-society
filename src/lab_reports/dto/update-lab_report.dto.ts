5aef8c4e5713f0d7ce5847ef876279e34e6727f7
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-23 17:10:55 +0545

    remove import trace

--- src/lab_reports/dto/update-lab_report.dto.ts
+++ src/lab_reports/dto/update-lab_report.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateLabReportDto } from './create-lab_report.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateLabReportDto extends PartialType(CreateLabReportDto) {}
