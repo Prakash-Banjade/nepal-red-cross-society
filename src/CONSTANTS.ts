a202f38e0828b2855fbac61a07250742f7aba4bc
Prakash Banjade <prakashbanjade191@gmail.com>
2024-06-11 14:53:17 +0545

    add constants

--- src/CONSTANTS.ts
+++ src/CONSTANTS.ts
@@ -2,4 +2,7 @@ export const CONSTANTS = {
     DONATION_INTERVAL: 90, // days
 
     BLOOD_EXPIRY_INTERVAL: 21 * 24 * 60 * 60 * 1000, // days * hours * minutes * seconds * milliseconds
+
+    BLOOD_BAG: 'Blood Bag',
+    SELF: 'SELF',
 }
