feecdfdcaf95b7812bd2af6b65246b7aa1c4a1f6
Prakash Banjade <prakashbanjade191@gmail.com>
2024-06-13 11:49:45 +0545

    made address module global

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Address } from './entities/address.entity';
 import { AddressService } from './address.service';
 
+@Global()
 @Module({
   imports: [
     TypeOrmModule.forFeature([Address]),
