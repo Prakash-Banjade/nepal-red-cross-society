8d7c512ee79c2c47a424749320e655c88064800b
Prakash Banjade <prakashbanjade191@gmail.com>
2024-06-21 14:24:14 +0545

    add municipality query

--- src/blood_request/dto/blood-request-query.dto.ts
+++ src/blood_request/dto/blood-request-query.dto.ts
@@ -2,6 +2,7 @@ import { ApiPropertyOptional } from "@nestjs/swagger";
 import { IsOptional } from "class-validator";
 import { QueryDto } from "src/core/dto/queryDto";
 import { BloodType, RhFactor } from "src/core/types/fieldsEnum.types";
+import { Municipal } from "src/core/types/municipals.types";
 
 export class BloodRequestQueryDto extends QueryDto {
     @ApiPropertyOptional({ type: 'enum', enum: BloodType, description: 'Donor blood type' })
@@ -11,4 +12,8 @@ export class BloodRequestQueryDto extends QueryDto {
     @ApiPropertyOptional({ type: 'enum', enum: RhFactor, description: 'Donor blood RH-factor' })
     @IsOptional()
     rhFactor?: RhFactor;
+
+    @ApiPropertyOptional({ type: 'enum', enum: Municipal, description: 'Hospital Municipal' })
+    @IsOptional()
+    municipality?: Municipal;
 }
