666bd5401a19e75231e4f441089922c52342066f
Prakash Banjade <prakashbanjade191@gmail.com>
2024-06-25 17:47:47 +0545

    fix: secondary contact optional

--- src/hospitals/dto/create-hospital.dto.ts
+++ src/hospitals/dto/create-hospital.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
 import { IsNotEmpty, IsOptional, IsPhoneNumber, IsString } from "class-validator";
 import { CreateAddressDto } from "src/address/dto/create-address.dto";
 
@@ -16,6 +17,7 @@ export class CreateHospitalDto extends CreateAddressDto {
 
     @ApiProperty()
     @IsPhoneNumber('NP')
+    @Transform(({ value }) => value?.length || null)
     @IsOptional()
     secondaryContact?: string
 }
